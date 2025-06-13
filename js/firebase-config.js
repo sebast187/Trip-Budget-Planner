8a8733aab15d6a33ec1240db96f30aa8dae8393e
Sebastián Quevedo <141648926+sebast187@users.noreply.github.com>
2025-06-12 23:28:44 -0600

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,17 @@
+// js/firebase-config.js
+
+// TODO: Replace with your project's actual Firebase SDK configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com", // Make sure this is correct
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database(); // Realtime Database
